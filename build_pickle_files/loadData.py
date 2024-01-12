6296dad859530f9e0d76320c65af2fc772c3c580
Joao Pedro <joopedrof.silva@gmail.com>
2024-01-11 22:27:41 -0300

    One file for each pickle batch to be created

--- build_pickle_files/loadData.py
+++ build_pickle_files/loadData.py
@@ -6,26 +6,15 @@ import matplotlib.pyplot as plt
 import numpy as np
 import pickle
 
-#matlab files
-matFilename_list = ['./data/2017-05-12_batchdata_updated_struct_errorcorrect.mat', 
-                    './data/2017-06-30_batchdata_updated_struct_errorcorrect.mat',
-                    './data/2018-04-12_batchdata_updated_struct_errorcorrect.mat']
-
-pkl_files = ['data/batch1.pkl', 'data/batch2.pkl', 'data/batch3.pkl']
-
-for k in range(len(pkl_files)):
-    
-    matFilename = matFilename_list[k]
-    pkl_file = pkl_files[k]
-    
+def loadData(matFilename, batchNum):  
     f = h5py.File(matFilename)
     batch = f['batch']
     
     num_cells = batch['summary'].shape[0]
     bat_dict = {}
     for i in range(num_cells):
-        cl = f[batch['cycle_life'][i,0]]
-        policy = f[batch['policy_readable'][i,0]]
+        cl = f[batch['cycle_life'][i,0]][()]
+        policy = f[batch['policy_readable'][i,0]][()]
         summary_IR = np.hstack(f[batch['summary'][i,0]]['IR'][0,:].tolist())
         summary_QC = np.hstack(f[batch['summary'][i,0]]['QCharge'][0,:].tolist())
         summary_QD = np.hstack(f[batch['summary'][i,0]]['QDischarge'][0,:].tolist())
@@ -42,21 +31,20 @@ for k in range(len(pkl_files)):
         cycle_dict = {}
 
         for j in range(cycles['I'].shape[0]):
-            I = np.hstack((f[cycles['I'][j,0]]))
-            Qc = np.hstack((f[cycles['Qc'][j,0]]))
-            Qd = np.hstack((f[cycles['Qd'][j,0]]))
-            Qdlin = np.hstack((f[cycles['Qdlin'][j,0]]))
-            T = np.hstack((f[cycles['T'][j,0]]))
-            Tdlin = np.hstack((f[cycles['Tdlin'][j,0]]))
-            V = np.hstack((f[cycles['V'][j,0]]))
-            dQdV = np.hstack((f[cycles['discharge_dQdV'][j,0]]))
-            t = np.hstack((f[cycles['t'][j,0]]))
+            I = np.hstack((f[cycles['I'][j,0]][()]))
+            Qc = np.hstack((f[cycles['Qc'][j,0]][()]))
+            Qd = np.hstack((f[cycles['Qd'][j,0]][()]))
+            Qdlin = np.hstack((f[cycles['Qdlin'][j,0]][()]))
+            T = np.hstack((f[cycles['T'][j,0]][()]))
+            Tdlin = np.hstack((f[cycles['Tdlin'][j,0]][()]))
+            V = np.hstack((f[cycles['V'][j,0]][()]))
+            dQdV = np.hstack((f[cycles['discharge_dQdV'][j,0]][()]))
+            t = np.hstack((f[cycles['t'][j,0]][()]))
             cd = {'I': I, 'Qc': Qc, 'Qd': Qd, 'Qdlin': Qdlin, 'T': T, 'Tdlin': Tdlin, 'V':V, 'dQdV': dQdV, 't':t}
             cycle_dict[str(j)] = cd
 
         cell_dict = {'cycle_life': cl, 'charge_policy':policy, 'summary': summary, 'cycles': cycle_dict}
-        key = f'b{k + 1}c' + str(i)
+        key = f'b{batchNum}c' + str(i)
         bat_dict[key]=   cell_dict
 
-    with open(pkl_file,'wb') as fp:
-        pickle.dump(bat_dict,fp, protocol=2)
+    return bat_dict
